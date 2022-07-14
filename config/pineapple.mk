b4929498c46c41320001a94829856eb32b0dfe0e
Ridhi Shah <ridhshah@qti.qualcomm.com>
2022-07-14 15:13:43 -0700

    1

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 dtbo-y := synx/pineapple-synx.dtbo
 dtbo-y += pineapple-synx-atp.dtbo
 dtbo-y += pineapple-synx-mtp.dtbo
 
+=======
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-sensor-cdp.dtbo \
+									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-qrd.dtbo
+>>>>>>> 6c31fdaa (ARM: dts: msm: Add camera devicetree for pineapple)
