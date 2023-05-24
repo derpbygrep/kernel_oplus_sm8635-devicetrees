edfa3da447bef8b324cb7b743cfebfd6735f96be
Soumen Ghosh <soumeng@quicinc.com>
2023-05-23 18:06:10 -0700

    ARM: dts: msm: camera: v2 overlay change to handle ipe lowsvs clk change
    This change handle the lowsvs clk value change of ipe for v2 device.
    
    CRs-Fixed: 3504948
    Change-Id: Ifca362e94adb22f0807db20df3482e2eeec332d3

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -5,7 +5,8 @@ dtbo-y += pineapple-synx-mtp.dtbo
 
 =======
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
-dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-sensor-cdp.dtbo \
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-overlay-v2.dtbo \
+									pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-qrd.dtbo
 >>>>>>> 6c31fdaa (ARM: dts: msm: Add camera devicetree for pineapple)
