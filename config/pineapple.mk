c22e300891d40eb2c376f1dd3cdcec3f90c912e7
Pengfei Liu <penliu@qti.qualcomm.com>
2023-07-31 14:53:17 +0800

    ARM: dts: msm: new hdk platform dtsi file
    Create HDK platform dtsi file and add related
    board ID and camera node.
    
    CRs-Fixed: 3543520
    External Impact: No.
    
    Change-Id: I145043e5d5d8a23004d3dac46cd4e442b121cffe
    (cherry picked from commit ffa3a1c4bb7aa7c2639d2f330c95988c7400e99e)

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -8,5 +8,6 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo
 >>>>>>> 6c31fdaa (ARM: dts: msm: Add camera devicetree for pineapple)
