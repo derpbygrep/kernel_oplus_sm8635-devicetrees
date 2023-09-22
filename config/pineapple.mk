8c553a50b075e69966a4e144cf8a2f4e9a01becd
Dharmender Sharma <dharshar@qti.qualcomm.com>
2023-09-22 12:31:13 +0530

    1

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -10,4 +10,8 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo
+<<<<<<< HEAD
 >>>>>>> 6c31fdaa (ARM: dts: msm: Add camera devicetree for pineapple)
+=======
+dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
+>>>>>>> c6bd8b8e (ARM: dts: msm: Add camera device tree for Cliffs)
