99b715bcf233e7eb7dd67351aa8e16a99a7f52cb
Pengfei Liu <penliu@qti.qualcomm.com>
2023-10-19 11:25:39 +0800

    1

--- config/pineapple.mk
+++ config/pineapple.mk
@@ -20,6 +20,7 @@ dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 =======
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 <<<<<<< HEAD
+<<<<<<< HEAD
 								cliffs-camera-sensor-mtp.dtbo
 >>>>>>> 6897dac7 (ARM: dts: msm: Add camera sensor DT for cliffs)
 =======
@@ -30,3 +31,7 @@ dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 								cliffs-camera-sensor-mtp.dtbo
 >>>>>>> 0b1b45fa (ARM: dts: msm: Add camera sensor DT for cliffs)
+=======
+								cliffs-camera-sensor-mtp.dtbo \
+								cliffs-camera-sensor-qrd.dtbo
+>>>>>>> 54cd0a4c (ARM: dts: msm: Add camera sensor DT for cliffs)
