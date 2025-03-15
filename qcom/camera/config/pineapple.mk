d60ac0d0235e7ed59638d677a1adfe8ed4571a66
Bruno Martins <bgcngm@gmail.com>
2025-03-15 15:13:09 +0530

    dt: Opt-out for irrelevant DTB[O]s based on config
    Change-Id: If2421afc876e320489d1e457b8ab91176e5f7b48
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -1,10 +1,14 @@
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
-# dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
-# 									pineapple-camera-sensor-cdp.dtbo \
-# 									pineapple-camera-sensor-mtp.dtbo \
-# 									pineapple-camera-sensor-hdk.dtbo \
-# 									pineapple-camera-sensor-qrd.dtbo \
-# 									pineapple-camera-sensor-aim500.dtbo
+ifneq ($(CONFIG_OPLUS_DEVICE_DTBS), y)
+dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
+									pineapple-camera-sensor-cdp.dtbo \
+									pineapple-camera-sensor-mtp.dtbo \
+									pineapple-camera-sensor-hdk.dtbo \
+									pineapple-camera-sensor-qrd.dtbo \
+									pineapple-camera-sensor-aim500.dtbo
+endif
+#OPLUS_DTS_OVERLAY start
+#OPLUS_DTS_OVERLAY end
 
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 
@@ -14,6 +18,11 @@ dtbo-$(CONFIG_ARCH_CLIFFS)    += oplus/lexus-camera-overlay.dtbo \
 # 								cliffs-camera-sensor-mtp.dtbo \
 # 								cliffs-camera-sensor-qrd.dtbo
 
+ifneq ($(CONFIG_OPLUS_DEVICE_DTBS), y)
+dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
+								cliffs-camera-sensor-mtp.dtbo \
+								cliffs-camera-sensor-qrd.dtbo
+endif
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera.dtbo
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-fp1-fp2.dtbo
 dtbo-$(CONFIG_ARCH_VOLCANO)     += volcano-camera-qrd.dtbo
